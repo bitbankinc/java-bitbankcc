eaab0743276d43603e4c075a3229e127e66a0aa4
dsaki <dsaki@users.noreply.github.com>
2017-04-13 11:16:37 +0900

    Example修正

--- src/main/java/cc/bitbank/Example.java
+++ src/main/java/cc/bitbank/Example.java
@@ -1,6 +1,7 @@
 package cc.bitbank;
 
 import cc.bitbank.entity.*;
+import cc.bitbank.entity.enums.CandleType;
 import cc.bitbank.entity.enums.CurrencyPair;
 import cc.bitbank.entity.enums.OrderSide;
 import cc.bitbank.entity.enums.OrderType;
@@ -8,6 +9,7 @@ import cc.bitbank.exception.BitbankException;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 
@@ -23,51 +25,55 @@ public class Example {
         bb.setKey(rb.getString("key"), rb.getString("secret"));
 
         try {
-//            bb.getTicker(CurrencyPair.BTC_JPY);
-//            bb.getDepth(CurrencyPair.BTC_JPY);
-//            bb.getTransaction(CurrencyPair.BTC_JPY).transactions[0].price
-//            bb.getTransaction(CurrencyPair.BTC_JPY, "20170410").transactions[0]
-//            bb.getCandlestick(CurrencyPair.BTC_JPY, CandleType._1DAY, "2017").candlestick[0].getOhlcvList()
+            Ticker ticker = bb.getTicker(CurrencyPair.BTC_JPY);
 
-//            Assets as = bb.getAsset();
-//            System.out.println(as.assets[0]);
-//
-//            Order order = bb.getOrder(CurrencyPair.BTC_JPY, 90956209);
-//            System.out.println(order);
-//
-//            Order order2 = bb.sendOrder(CurrencyPair.BTC_JPY, 10000, BigDecimal.valueOf(0.01), OrderSide.BUY, OrderType.LIMIT);
-//            System.out.println(order2);
+            Depth depth = bb.getDepth(CurrencyPair.BTC_JPY);
 
-//            Order order3 = bb.cancelOrder(CurrencyPair.BTC_JPY, 129781978);
-//            System.out.println(order3);
+            Transactions ts = bb.getTransaction(CurrencyPair.BTC_JPY);
 
-//            long[] ids = {129830841, 129830734};
-//            Orders orders = bb.cancelOrders(CurrencyPair.BTC_JPY, ids);
-//            System.out.println(orders.orders[0]);
-//            System.out.println(orders.orders[1]);
+            Transactions.Transaction[] ts2 = bb.getTransaction(CurrencyPair.BTC_JPY, "20170410").transactions;
 
-//            long[] ids = {90956209, 90951996};
-//            Orders orders = bb.getOrders(CurrencyPair.BTC_JPY, ids);
-//            System.out.println(orders.orders[0]);
-//            System.out.println(orders.orders[1]);
+            List<Candlestick.Ohlcvs.Ohlcv> cs = bb.getCandlestick(CurrencyPair.BTC_JPY, CandleType._1DAY, "2017").candlestick[0].getOhlcvList();
 
-//            Map<String, Long> option = new HashMap();
-//            option.put("count", 1L);
-//            option.put("since", 1490348550380L);
-//            // Option's parameter can be seen https://docs.bitbank.cc/#!/Order/active_orders
-//            Orders orders = bb.getActiveOrders(CurrencyPair.BTC_JPY, option);
-//            for(Order o : orders.orders) {
-//                System.out.println(o);
-//            }
+            Assets as = bb.getAsset();
+            System.out.println(as.assets[0]);
 
-//            Accounts accounts = bb.getWithdrawalAccounts("btc");
-//            for(Accounts.Account a : accounts.accounts) {
-//                System.out.println(a);
-//            }
+            Order order = bb.getOrder(CurrencyPair.BTC_JPY, 90956209);
+            System.out.println(order);
+
+            Order order2 = bb.sendOrder(CurrencyPair.BTC_JPY, 10000, BigDecimal.valueOf(0.01), OrderSide.BUY, OrderType.LIMIT);
+            System.out.println(order2);
+
+            Order order3 = bb.cancelOrder(CurrencyPair.BTC_JPY, 129781978);
+            System.out.println(order3);
+
+            long[] ids = {129830841, 129830734};
+            Orders orders = bb.cancelOrders(CurrencyPair.BTC_JPY, ids);
+            System.out.println(orders.orders[0]);
+            System.out.println(orders.orders[1]);
+
+            long[] ids2 = {90956209, 90951996};
+            Orders orders2 = bb.getOrders(CurrencyPair.BTC_JPY, ids2);
+            System.out.println(orders2.orders[0]);
+            System.out.println(orders2.orders[1]);
+
+            Map<String, Long> option = new HashMap();
+            option.put("count", 1L);
+            option.put("since", 1490348550380L);
+            // Option's parameter can be seen https://docs.bitbank.cc/#!/Order/active_orders
+            Orders orders3 = bb.getActiveOrders(CurrencyPair.BTC_JPY, option);
+            for(Order o : orders3.orders) {
+                System.out.println(o);
+            }
+
+            Accounts accounts = bb.getWithdrawalAccounts("btc");
+            for(Accounts.Account a : accounts.accounts) {
+                System.out.println(a);
+            }
 
             Withdraw w = bb.requestWithdraw("btc", "XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXX",
                     BigDecimal.valueOf(0.005), "867005", "");
-            System.out.println(w.status);
+            System.out.println(w);
 
 
         } catch (BitbankException e) {
