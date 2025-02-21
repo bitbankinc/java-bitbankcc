c749e91dcb493116eec891f8779dd0f769f8b9d4
kohei.iizuka <kohei.iizuka@bitcoinbank.co.jp>
2025-02-21 12:53:10 +0900

    fix: margin positions toString()

--- src/main/java/cc/bitbank/entity/MarginPositions.java
+++ src/main/java/cc/bitbank/entity/MarginPositions.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class MarginPositions extends Data {
     public Notice notice;
     public Payable payables;
-    public Position[] positions;
+    public List<Position> positions;
     @JsonProperty("losscut_threshold")
     public LosscutThreshold losscutThreshold;
 
@@ -20,10 +22,18 @@ public class MarginPositions extends Data {
         public BigDecimal amount;
         @JsonProperty("due_date_at")
         public Long dueDateAt;
+
+        public String toString() {
+            return "{ what: " + what + ", occurredAt: " + occurredAt + ", amount: " + amount + ", dueDateAt: " + dueDateAt + " }";
+        }
     }
 
     public static class Payable {
         public BigDecimal amount;
+
+        public String toString() {
+            return "{ amount: " + amount + " }";
+        }
     }
 
     public static class Position {
@@ -39,14 +49,22 @@ public class MarginPositions extends Data {
         public BigDecimal unrealizedFeeAmount;
         @JsonProperty("unrealized_interest_amount")
         public BigDecimal unrealizedInterestAmount;
+
+        public String toString() {
+            return "{ pair: " + pair + ", positionSide: " + positionSide + ", openAmount: " + openAmount + ", product: " + product + ", averagePrice: " + averagePrice + ", unrealizedFeeAmount: " + unrealizedFeeAmount + ", unrealizedInterestAmount: " + unrealizedInterestAmount + " }";
+        }
     }
 
     public static class LosscutThreshold {
         public String company;
         public String individual;
+
+        public String toString() {
+            return "{ company: " + company + ", individual: " + individual + " }";
+        }
     }
 
     public String toString() {
-        return "[MarginPositions] notice " + notice + ", payables " + payables + ", positions " + positions + ", losscutThreshold " + losscutThreshold;
+        return "[MarginPositions] notice " + notice.toString() + ", payables " + payables.toString() + ", positions " + positions.stream().map(p -> p.toString()).collect(Collectors.joining(", ")) + ", losscutThreshold " + losscutThreshold.toString();
     }
 }
